53c8fd7b7024d12b8dc5d3a0f75f847e73739a36
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2026-07-29 19:39:38 +0100

    feat: normaliza dados de km e preço após importação

--- train.py
+++ train.py
@@ -9,9 +9,31 @@ with open('data.csv', mode='r') as file:
     next(reader)
     
     for row in reader:
-        mileages.append(row[0])
-        prices.append(row[1])
+        mileages.append(float(row[0]))
+        prices.append(float(row[1]))
         
 
 print("Kms importados: ", mileages)
 print("Precos importados: ", prices)
+
+norm_mileages = []
+norm_prices = []
+
+min_mileage = min(mileages)
+max_mileage = max(mileages)
+
+min_price = min(prices)
+max_price = max(prices)
+
+for km in mileages:
+    norm_km = (km - min_mileage) / (max_mileage - min_mileage)
+    norm_mileages.append(norm_km)
+    
+for pc in prices:
+    norm_pc = (pc - min_price) / (max_price - min_price)
+    norm_prices.append(norm_pc)
+    
+print("\nDados normalizados:\n")
+print("kms normalizados: ", norm_mileages)
+print()
+print("Precos normalizados: ", norm_prices)
