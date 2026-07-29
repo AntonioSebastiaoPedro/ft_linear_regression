76e1b3ddab83a2780877b768f5eb90770ba15596
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2026-07-29 20:53:18 +0100

    feat: adiciona implementação da descida do gradiente para treinamento do modelo

--- train.py
+++ train.py
@@ -36,3 +36,35 @@ for pc in prices:
 print("\n--- Dados Normalizados ---")
 print("Primeiras 3 Quilometragens:", norm_mileages[:5])
 print("Primeiros 3 Preços:", norm_prices[:5])
+
+
+# --- Inicio da descida do gradiente ---
+learning_rate = 0.1
+epochs = 1000
+theta0 = 0.0
+theta1 = 0.0
+m = len(norm_mileages)
+
+print("\nIniciando o treinamento...")
+
+for time in range(epochs):
+    sum_error_theta0 = 0
+    sum_error_theta1 = 0
+    
+    for i in range(m):
+        estimate_price = theta0 + (theta1 * norm_mileages[i])
+        error = estimate_price - norm_prices[i]
+        sum_error_theta0 += error
+        sum_error_theta1 += error * norm_mileages[i]
+        
+    temp_theta0 = learning_rate * (1/m) * sum_error_theta0
+    temp_theta1 = learning_rate * (1/m) * sum_error_theta1
+    
+    theta0 = theta0 - temp_theta0
+    theta1 = theta1 - temp_theta1
+    
+# --- FIM DO TREINAMENTO ---
+
+print("Treinamento concluido com sucesso")
+print(f"Theta0 final (normalizado): {theta0}")
+print(f"Theta1 final (normalizado): {theta1}")
